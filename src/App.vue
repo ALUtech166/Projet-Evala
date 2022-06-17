3f8c4c2ebe5e749dd9884e3656c32c390befde59
ALUtech166 <s.tassiga@alustudent.com>
2022-06-17 12:34:49 +0000

    deploy

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,27 @@
 
   <div class="container-fluid">
     <header-bar></header-bar>
-  <div class="app">
+    <div class="app">
 
       <div class="login-popup">
-      <div class="box">
-        <div class="container">
-          <img src="./components/images/pub.jpg" alt="" class="img-fluid">
-        </div>
-        <div class="form">
-          <div class="close">&times;</div>
-            
+        <div class="box">
+          <div class="container">
+            <img src="./components/images/pub.jpg" alt="" class="img-fluid">
+          </div>
+          <div class="form">
+            <div class="close">&times;</div>
+
+          </div>
         </div>
       </div>
+
+
+
+
+      <router-view />
     </div>
-    
 
-
-
-    <router-view />
-  </div>
-
-  <footer-bar></footer-bar>
+    <footer-bar></footer-bar>
 
   </div>
 </template>
@@ -30,8 +30,10 @@
 <script>
   import HeaderBar from './components/partials/HeaderBar.vue'
   import FooterBar from './components/partials/FooterBar.vue'
-  import {mapGetters} from 'vuex'
- 
+  import {
+    mapGetters
+  } from 'vuex'
+
 
 
   export default {
@@ -42,215 +44,212 @@
     },
 
     computed: {
-    ...mapGetters(['user'])
-  },
+      ...mapGetters(['user'])
+    },
 
-  mounted() {
-     const loginPopup = document.querySelector(".login-popup");
-  const close = document.querySelector(".close");
-
-
-  window.addEventListener("load",function(){
- 
-   showPopup();
-   // setTimeout(function(){
-   //   loginPopup.classList.add("show");
-   // },5000)
-
-  })
-
-  function showPopup(){
-        const timeLimit = 5 // seconds;
-        let i=0;
-        const timer = setInterval(function(){
-         i++;
-         if(i == timeLimit){
-          clearInterval(timer);
-          loginPopup.classList.add("show");
-         } 
-         console.log(i)
-        },1000);
-  }
+    mounted() {
+      const loginPopup = document.querySelector(".login-popup");
+      const close = document.querySelector(".close");
 
 
-  close.addEventListener("click",function(){
-    loginPopup.classList.remove("show");
-  })
+      window.addEventListener("load", function () {
+
+        showPopup();
+        setTimeout(function(){
+        loginPopup.classList.add("show");
+        },5000)
+
+      })
+
+      function showPopup() {
+        const timeLimit = 1 // seconds;
+        let i = 0;
+        const timer = setInterval(function () {
+          i++;
+          if (i == timeLimit) {
+            clearInterval(timer);
+            loginPopup.classList.add("show");
+          }
+          console.log(i)
+        }, 1000);
+      }
 
 
-  },
+      close.addEventListener("click", function () {
+        loginPopup.classList.remove("show");
+      })
+
+
+    },
 
 
   }
 </script>
 
 <style>
-@import './global-style.css';
+  @import './global-style.css';
 
 
- body {
-        font-family: sans-serif;
-        margin: 0;
-        line-height: 1.5;
-      }
+  body {
+    font-family: sans-serif;
+    margin: 0;
+    line-height: 1.5;
+  }
 
-      * {
-        box-sizing: border-box;
-        margin: 0;
-      }
+  * {
+    box-sizing: border-box;
+    margin: 0;
+  }
 
-      .login-popup {
-        position: fixed;
-        left: 0;
-        top: 0;
-        width: 100%;
-        height: 100%;
-        z-index: 1099;
-        background-color: rgba(0, 0, 0, 0.6);
-        visibility: hidden;
-        opacity: 0;
-        transition: all 1s ease;
-      }
+  .login-popup {
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 1099;
+    background-color: rgba(0, 0, 0, 0.6);
+    visibility: hidden;
+    opacity: 0;
+    transition: all 1s ease;
+  }
 
-      .login-popup.show {
-        visibility: visible;
-        opacity: 1;
-      }
+  .login-popup.show {
+    visibility: visible;
+    opacity: 1;
+  }
 
-      .login-popup .box {
-        background-color: #ffffff;
-        width: 750px;
-        position: absolute;
-        left: 50%;
-        top: 50%;
-        transform: translate(-50%, -50%);
-        display: flex;
-        flex-wrap: wrap;
-        opacity: 0;
-        margin-left: 50px;
-        transition: all 1s ease;
+  .login-popup .box {
+    background-color: #ffffff;
+    width: 750px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    display: flex;
+    flex-wrap: wrap;
+    opacity: 0;
+    margin-left: 50px;
+    transition: all 1s ease;
 
-      }
+  }
 
-      .login-popup.show .box {
-        opacity: 1;
-        margin-left: 0;
-      }
+  .login-popup.show .box {
+    opacity: 1;
+    margin-left: 0;
+  }
 
-      .login-popup .box .img-area {
-        flex: 0 0 50%;
-        max-width: 50%;
-        position: relative;
-        overflow: hidden;
-        padding: 30px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-      }
+  .login-popup .box .img-area {
+    flex: 0 0 50%;
+    max-width: 50%;
+    position: relative;
+    overflow: hidden;
+    padding: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 
-      .login-popup .box .img-area h1 {
-        font-size: 30px;
-      }
+  .login-popup .box .img-area h1 {
+    font-size: 30px;
+  }
 
-      .login-popup .box .img-area .img {
-        position: absolute;
-        left: 0;
-        top: 0;
-        width: 100%;
-        height: 100%;
-        background-image: url('../public/2.jpg');
-        background-size: cover;
-        background-position: center;
-        animation: zoomInOut 7s linear infinite;
-        z-index: -1;
+  .login-popup .box .img-area .img {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-image: url('../public/2.jpg');
+    background-size: cover;
+    background-position: center;
+    animation: zoomInOut 7s linear infinite;
+    z-index: -1;
 
-      }
+  }
 
-      @keyframes zoomInOut {
+  @keyframes zoomInOut {
 
-        0%,
-        100% {
-          transform: scale(1);
-        }
+    0%,
+    100% {
+      transform: scale(5);
+    }
+    
 
-        50% {
-          transform: scale(1.1);
-        }
-      }
+    50% {
+      transform: scale(2);
+    }
+  }
 
-      .login-popup .box .form {
-        flex: 0 0 50%;
-        max-width: 50%;
-        padding: 40px 30px;
-      }
+  .login-popup .box .form {
+    flex: 0 0 50%;
+    max-width: 50%;
+    padding: 40px 30px;
+  }
 
-      .login-popup .box .form h1 {
-        color: #000000;
-        font-size: 30px;
-        margin: 0 0 30px;
-      }
+  .login-popup .box .form h1 {
+    color: #000000;
+    font-size: 30px;
+    margin: 0 0 30px;
+  }
 
-      .login-popup .box .form .form-control {
-        height: 45px;
-        margin-bottom: 30px;
-        width: 100%;
-        border: none;
-        border-bottom: 1px solid #cccccc;
-        font-size: 15px;
-        color: #000000;
-      }
+  .login-popup .box .form .form-control {
+    height: 45px;
+    margin-bottom: 30px;
+    width: 100%;
+    border: none;
+    border-bottom: 1px solid #cccccc;
+    font-size: 15px;
+    color: #000000;
+  }
 
-      .login-popup .box .form .form-control:focus {
-        outline: none;
-      }
+  .login-popup .box .form .form-control:focus {
+    outline: none;
+  }
 
-      .login-popup .box .form label {
-        font-size: 15px;
-        color: #555555;
-      }
+  .login-popup .box .form label {
+    font-size: 15px;
+    color: #555555;
+  }
 
-      .login-popup .box .form .btn {
-        width: 100%;
-        background-color: #E91E63;
-        margin-top: 40px;
-        height: 45px;
-        border: none;
-        border-radius: 25px;
-        font-size: 15px;
-        text-transform: uppercase;
-        color: #ffffff;
-        cursor: pointer;
-      }
+  .login-popup .box .form .btn {
+    width: 100%;
+    background-color: #E91E63;
+    margin-top: 40px;
+    height: 45px;
+    border: none;
+    border-radius: 25px;
+    font-size: 15px;
+    text-transform: uppercase;
+    color: #ffffff;
+    cursor: pointer;
+  }
 
-      .login-popup .box .form .btn:focus {
-        outline: none;
-      }
+  .login-popup .box .form .btn:focus {
+    outline: none;
+  }
 
-      .login-popup .box .form .close {
-        position: absolute;
-        right: 10px;
-        top: 0px;
-        font-size: 30px;
-        cursor: pointer;
-      }
+  .login-popup .box .form .close {
+    position: absolute;
+    right: 10px;
+    top: 0px;
+    font-size: 30px;
+    cursor: pointer;
+  }
 
-      /*responsive*/
-      @media(max-width: 767px) {
-        .login-popup .box {
-          width: calc(100% - 30px);
-        }
+  /*responsive*/
+  @media(max-width: 767px) {
+    .login-popup .box {
+      width: calc(100% - 30px);
+    }
 
-        .login-popup .box .img-area {
-          display: none;
-        }
+    .login-popup .box .img-area {
+      display: none;
+    }
 
-        .login-popup .box .form {
-          flex: 0 0 100%;
-          max-width: 100%;
-        }
-      }
-
+    .login-popup .box .form {
+      flex: 0 0 100%;
+      max-width: 100%;
+    }
+  }
 </style>
-
-
-
